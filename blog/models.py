fcbb85d92022501415f59812cd063c13226d7d21
20151772 <kmaengggong@gmail.com>
2021-11-29 23:28:01 +0900

    미리보기와 요약문 추가

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)

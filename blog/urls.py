b13b505cc53a9930e8cfa7784c2664c321f5bafb
20151772 <kmaengggong@gmail.com>
2021-12-01 21:21:15 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.index),
-    #path('<int:pk>/', views.single_post_page),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('category/<str:slug>/', views.category_page)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
 
@@ -22,26 +23,22 @@ class PostDetail(DetailView):
 
         return context
 
-'''
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
     return render(
         request,
         'blog/post_list.html',
         {
-            'posts': posts,
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
         }
     )
-
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/post_detail.html',
-        {
-            'post': post,
-        }
-    )
-'''

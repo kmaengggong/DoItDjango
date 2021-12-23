dc51691b48d5785ec413e9cc09b2b39dcb0c4d35
kmaengggong-5600g <kmaengggong@gmail.com>
2021-12-24 00:15:48 +0900

    글 작성 페이지 생성

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('category/<str:slug>/', views.category_page),
-    path('tag/<str:slug>/', views.tag_page)
+    path('tag/<str:slug>/', views.tag_page),
+    path('create_post/', views.PostCreate.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post, Category, Tag
 
 class PostList(ListView):
@@ -22,6 +22,10 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
 
         return context
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
 def category_page(request, slug):
     if slug == 'no_category':

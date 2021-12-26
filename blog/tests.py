e4bcf11d8b116a32ce30f2b10f3cbcf7edf6cb95
kmaengggong-5600g <kmaengggong@gmail.com>
2021-12-27 06:45:26 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/tests.py
+++ blog/tests.py
@@ -167,6 +167,11 @@ class TestView(TestCase):
         self.assertNotIn(self.post_003.title, main_area.text)
 
     def test_create_post(self):
+        response = self.client.get('/blog/create_post/')
+        self.assertNotEqual(response.status_code, 200)
+
+        self.client.login(username='trump', pssword='somepassword')
+
         response = self.client.get('/blog/create_post')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -174,3 +179,14 @@ class TestView(TestCase):
         self.assertEqual('Create Post - Blog', soup.title.text)
         main_area = soup.find('div', id='main-area')
         self.assertIn('Create New Post', main_area.text)
+
+        self.client.post(
+            '/blog/create_post/',
+            {
+                'title': 'Post Form 만들기',
+                'content': 'Post Form 페이지를 만듭시다.'
+            }
+        )
+        last_post = Post.objects.last()
+        self.assertEqual(last_post.title, 'Post Form 만들기')
+        self.assertEqual(last_post.author.username, 'trump')

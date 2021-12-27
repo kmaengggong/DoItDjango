64d57fdf025aa97b3617efce192d6cc9b97ee734
kmaengggong-5600g <kmaengggong@gmail.com>
2021-12-27 13:45:06 +0900

    포스트를 수정할 때 텍스트로 태그 지정

--- blog/tests.py
+++ blog/tests.py
@@ -226,12 +226,17 @@ class TestView(TestCase):
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
                 'title': '세 번째 포스트를 수정했습니다.',
                 'content': 'DOTA WTF',
-                'category': self.category_programming.pk
+                'category': self.category_programming.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag'
             },
             follow=True
         )
@@ -240,3 +245,7 @@ class TestView(TestCase):
         self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
         self.assertIn('DOTA WTF', main_area.text)
         self.assertIn(self.category_programming.name, main_area.text)
+        self.assertIn('파이썬 공부',  main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)

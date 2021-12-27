d25cea204ad6324b5168638941896d101f778d59
kmaengggong-5600g <kmaengggong@gmail.com>
2021-12-27 14:26:31 +0900

    Cotent 입력란에 마크다운 적용

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from markdownx.models import MarkdownxField
+from markdownx.utils import markdown
 import os
 
 class Tag(models.Model):
@@ -28,7 +30,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
-    content = models.TextField()
+    content = MarkdownxField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -53,3 +55,6 @@ class Post(models.Model):
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
+
+    def get_content_markdown(self):
+        return markdown(self.content)
--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('markdownx/', include('markdownx.urls')),
     path('', include('single_pages.urls'))
 ]
 

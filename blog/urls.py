c8c27c3f2e9e743c9e524808596c6b5786b39d46
kmaengggong-5600g <kmaengggong@gmail.com>
2022-01-02 16:10:13 +0900

    댓글 폼으로 상세 페이지에서 댓글 작성

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
+    path('<int:pk>/new_comment/', views.new_comment),
     path('category/<str:slug>/', views.category_page),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),

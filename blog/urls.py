78e9e26d8c2f7b57dc772f2d286bd7b7a8e70e39
kmaengggong-5600g <kmaengggong@gmail.com>
2022-01-03 18:16:55 +0900

    검색 기능 구현

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:pk>/new_comment/', views.new_comment),
     path('category/<str:slug>/', views.category_page),
     path('delete_comment/<int:pk>/', views.delete_comment),
+    path('search/<str:q>/', views.PostSearch.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),

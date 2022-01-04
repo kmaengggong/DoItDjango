ba5b3a0a3bcf43658ed95be654af9f3c4659233d
kmaengggong-5600g <kmaengggong@gmail.com>
2022-01-04 13:53:31 +0900

    도커에 쟝고 담기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -9,13 +9,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!!j%)!egvd(#2euq)fi0=+ukymkolsv_)6u1@q8z9l0sot_)%g'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-!!j%)!egvd(#2euq)fi0=+ukymkolsv_)6u1@q8z9l0sot_)%g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
-
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOST = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 # Application definition
 
